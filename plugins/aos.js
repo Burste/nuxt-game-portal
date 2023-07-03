77e476dc042a4a8bd940c499a7d39dfde0e64fd5
Elek <sppqre@gmail.com>
2023-07-03 21:19:56 +0800

    延遲 AOS init 時機

--- plugins/aos.js
+++ plugins/aos.js
@@ -3,10 +3,12 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 
 export default defineNuxtPlugin((nuxtApp) => {
-    if (typeof window !== 'undefined') {
-        nuxtApp.AOS = AOS.init({
-            once: false,
-            duration: 400, // values from 0 to 3000, with step 50ms
-        });
-    }
+    nuxtApp.hook('page:finish', () => {
+        if (typeof window !== 'undefined') {
+            nuxtApp.AOS = AOS.init({
+                once: false,
+                duration: 400, // values from 0 to 3000, with step 50ms
+            });
+        }
+    });
 });
